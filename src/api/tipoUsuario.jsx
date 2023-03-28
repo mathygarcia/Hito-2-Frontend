e31b903f139551e5323c5ce20e2f86270fdfb8df
Mathias Garcia <zmathias.garciaz@gmail.com>
2023-03-28 12:32:56 -0300

    ajuste

--- src/api/tipoUsuario.jsx
+++ src/api/tipoUsuario.jsx
@@ -3,13 +3,3 @@ export const typeUsu = async () => {
     const data = await res.json();
     return data;
 }
-/* import axios from "axios";
-export const typeUsu = async () => {
-  try {
-    const res = await axios.get("http://localhost:3001/tipo-usuarios");
-    return res;
-  } catch (error) {
-    console.log("typeUsuError", error);
-    throw new Error("no hay tipos");
-  }
-}; */
